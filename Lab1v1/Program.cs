37c467494d4b913e03a5d4d82e042219683f64a6
Rise <ritm-temp@hotmail.com>
2018-02-13 10:48:53 +0600

    Lab1

---
+++ Lab1v1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysInYear yearConverter = new DaysInYear();
+            Console.WriteLine("Пожалуйста, введите год");
+            string year = Console.ReadLine();
+            int days = yearConverter.NumberOfDaysInYear(year);
+            Console.WriteLine($"Число дней в {year} году равно {days}");
+            Console.ReadLine();
+        }
+    }
+}
+
